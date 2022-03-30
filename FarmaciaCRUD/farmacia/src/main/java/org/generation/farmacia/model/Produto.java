4829d61c1d95bb98f653c49f4372992ae0f4cff1
unknown <geordansantos@gmail.com>
2022-03-30 20:32:05 -0300

    CRUD de uma Farmácia

---
+++ FarmaciaCRUD/farmacia/src/main/java/org/generation/farmacia/model/Produto.java
@@ -0,0 +1,86 @@
+package org.generation.farmacia.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity //trasforma em tabela
+@Table(name="tb_produtos")
+	
+public class Produto {
+	
+	@Id // define o id, pk
+	@GeneratedValue(strategy=GenerationType.IDENTITY)//auto increment
+	private Long id;
+
+	@NotNull
+	@Size(min=2, max=50)
+	private String nome;
+	
+	@NotNull
+	@Size(min=2, max=50)
+	private String marca;
+	
+	@NotNull
+	private Double preço;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getMarca() {
+		return marca;
+	}
+
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
+	
+	
+	
+
+
+	
+	
+}
