87fb2ec3a37e95dc30ba7586cadbd7f797f01b77
unknown <geordansantos@gmail.com>
2022-03-31 12:58:35 -0300

    CRUD de uma Loja de games

---
+++ LojaGamesCRUD/lojagames/src/main/java/org/generation/lojagames/model/Produto.java
@@ -0,0 +1,79 @@
+package org.generation.lojagames.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name="tb_produtos")
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull
+	@Size(min=2, max=50)
+	private String nome;
+	
+	@NotNull
+	@Size(min=2, max=50)
+	private String plataforma;
+	
+	@NotNull
+	private Double preço;
+	
+	@ManyToOne
+	@JsonIgnoreProperties("produto")
+	private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getPlataforma() {
+		return plataforma;
+	}
+
+	public void setPlataforma(String marca) {
+		this.plataforma = marca;
+	}
+
+	public Double getPreço() {
+		return preço;
+	}
+
+	public void setPreço(Double preço) {
+		this.preço = preço;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+}
